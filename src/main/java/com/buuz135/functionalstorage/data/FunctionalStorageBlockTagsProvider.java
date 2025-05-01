d048c51524d6dbbdf98dff13f152baf1d39df8fb
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 22:03:20 +0900

    fixed: FunctionalStorageBlockTagsProvider

--- src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageBlockTagsProvider.java
+++ src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageBlockTagsProvider.java
@@ -1,15 +1,15 @@
 package com.buuz135.functionalstorage.data;
 
 import com.buuz135.functionalstorage.FunctionalStorage;
+import net.minecraft.block.Block;
+import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.fml.RegistryObject;
 import org.apache.commons.lang3.tuple.Pair;
-import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nullable;
 import java.util.stream.Collectors;
 
 public class FunctionalStorageBlockTagsProvider extends BlockTagsProvider {
@@ -20,7 +20,7 @@ public class FunctionalStorageBlockTagsProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags() {
-        TagAppender<Block> tTagAppender = this.tag(BlockTags.MINEABLE_WITH_AXE);
+        Builder<Block> tTagAppender = this.tag(BlockTags.MINEABLE_WITH_AXE);
         for (FunctionalStorage.DrawerType drawerType : FunctionalStorage.DRAWER_TYPES.keySet()) {
             for (RegistryObject<Block> blockRegistryObject : FunctionalStorage.DRAWER_TYPES.get(drawerType).stream().map(Pair::getLeft).collect(Collectors.toList())) {
                 tTagAppender.add(blockRegistryObject.get());
