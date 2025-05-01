a29790f7a38ebff0c3f21d7f0422550541f90dc5
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-02 08:53:02 +0900

    fixed: IQuadTransformer

--- src/main/java/com/buuz135/functionalstorage/client/loader/IQuadTransformer.java
+++ src/main/java/com/buuz135/functionalstorage/client/loader/IQuadTransformer.java
@@ -1,20 +1,20 @@
 package com.buuz135.functionalstorage.client.loader;
 
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.VertexFormatElement;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormatElement;
 
 public class IQuadTransformer {
 
-    public static int STRIDE = DefaultVertexFormat.BLOCK.getIntegerSize();
-    public static int POSITION = findOffset(DefaultVertexFormat.ELEMENT_POSITION);
-    public static int COLOR = findOffset(DefaultVertexFormat.ELEMENT_COLOR);
-    public static int UV0 = findOffset(DefaultVertexFormat.ELEMENT_UV0);
-    public static int UV1 = findOffset(DefaultVertexFormat.ELEMENT_UV1);
-    public static int UV2 = findOffset(DefaultVertexFormat.ELEMENT_UV2);
-    public static int NORMAL = findOffset(DefaultVertexFormat.ELEMENT_NORMAL);
+    public static int STRIDE = DefaultVertexFormats.BLOCK.getIntegerSize();
+    public static int POSITION = findOffset(DefaultVertexFormats.ELEMENT_POSITION);
+    public static int COLOR = findOffset(DefaultVertexFormats.ELEMENT_COLOR);
+    public static int UV0 = findOffset(DefaultVertexFormats.ELEMENT_UV0);
+    public static int UV1 = findOffset(DefaultVertexFormats.ELEMENT_UV1);
+    public static int UV2 = findOffset(DefaultVertexFormats.ELEMENT_UV2);
+    public static int NORMAL = findOffset(DefaultVertexFormats.ELEMENT_NORMAL);
 
     private static int findOffset(VertexFormatElement element) {
-        int index = DefaultVertexFormat.BLOCK.getElements().indexOf(element);
-        return index < 0 ? -1 : DefaultVertexFormat.BLOCK.getOffset(index) / 4;
+        int index = DefaultVertexFormats.BLOCK.getElements().indexOf(element);
+        return index < 0 ? -1 : DefaultVertexFormats.BLOCK.getOffset(index) / 4;
     }
 }
