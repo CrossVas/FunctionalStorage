4faec498dd6cfa1ef803cc6068e526aef0d9b5e7
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 21:19:03 +0900

    fixed: MathUtils

--- src/main/java/com/buuz135/functionalstorage/util/MathUtils.java
+++ src/main/java/com/buuz135/functionalstorage/util/MathUtils.java
@@ -1,12 +1,12 @@
 package com.buuz135.functionalstorage.util;
 
-import com.mojang.math.Matrix4f;
-import com.mojang.math.Quaternion;
-import com.mojang.math.Vector3f;
+import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Quaternion;
+import net.minecraft.util.math.vector.Vector3f;
 
 public class MathUtils {
     public static Matrix4f createTransformMatrix(Vector3f translation, Vector3f eulerDegrees, Vector3f scale) {
-        Quaternion q = Quaternion.fromXYZDegrees(eulerDegrees);
+        Quaternion q = fromXYZDegrees(eulerDegrees);
         return createTransformMatrix(translation, q, scale);
     }
 
@@ -24,5 +24,17 @@ public class MathUtils {
 
     public static Matrix4f createTransformMatrix(Vector3f translation, Quaternion rotation, float scale) {
         return createTransformMatrix(translation, rotation, new Vector3f(scale, scale, scale));
+    }
+
+    public static Quaternion fromXYZDegrees(Vector3f pDegreesVector) {
+        return fromXYZ((float) Math.toRadians(pDegreesVector.x()), (float) Math.toRadians(pDegreesVector.y()), (float) Math.toRadians(pDegreesVector.z()));
+    }
+
+    public static Quaternion fromXYZ(float pX, float pY, float pZ) {
+        Quaternion quaternion = Quaternion.ONE.copy();
+        quaternion.mul(new Quaternion((float) Math.sin(pX / 2.0F), 0.0F, 0.0F, (float) Math.cos(pX / 2.0F)));
+        quaternion.mul(new Quaternion(0.0F, (float) Math.sin(pY / 2.0F), 0.0F, (float) Math.cos(pY / 2.0F)));
+        quaternion.mul(new Quaternion(0.0F, 0.0F, (float) Math.sin(pZ / 2.0F), (float) Math.cos(pZ / 2.0F)));
+        return quaternion;
     }
 }
