7e9e88a358c27c64fcde4de4cba4016ed8767aa5
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 22:00:45 +0900

    fixed: FunctionalStorageLangProvider

--- src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageLangProvider.java
+++ src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageLangProvider.java
@@ -3,11 +3,11 @@ package com.buuz135.functionalstorage.data;
 import com.buuz135.functionalstorage.FunctionalStorage;
 import com.buuz135.functionalstorage.block.DrawerBlock;
 import com.buuz135.functionalstorage.item.StorageUpgradeItem;
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.LanguageProvider;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.apache.commons.lang3.text.WordUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
