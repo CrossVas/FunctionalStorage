2093c23409c65750fc2394ae927baa166aed8ca1
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 21:15:36 +0900

    fixed: TooltipUtil

--- src/main/java/com/buuz135/functionalstorage/util/TooltipUtil.java
+++ src/main/java/com/buuz135/functionalstorage/util/TooltipUtil.java
@@ -1,20 +1,21 @@
 package com.buuz135.functionalstorage.util;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.ChatFormatting;
+import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.container.PlayerContainer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -23,30 +24,32 @@ import java.util.List;
 
 public class TooltipUtil {
 
+    private static final Vector3f DIFFUSE_LIGHT_0 = Util.make(new Vector3f(0.2f, 1.0f, -0.7f), Vector3f::normalize);
+    private static final Vector3f DIFFUSE_LIGHT_1 = Util.make(new Vector3f(-0.2f, 1.0f, 0.7f), Vector3f::normalize);
 
-    public static void renderItems(PoseStack stack, List<ItemStack> items, int x, int y) {
+    public static void renderItems(MatrixStack stack, List<ItemStack> items, int x, int y) {
         for (int i = 0; i < items.size(); i++) {
             renderItemIntoGUI(stack, items.get(i), x + 18 * i, y, 512);
         }
     }
 
-    public static void renderItemIntoGUI(PoseStack matrixStack, ItemStack stack, int x, int y, int z) {
-        renderItemModelIntoGUI(matrixStack, stack, x, y, z, Minecraft.getInstance().getItemRenderer().getModel(stack, (Level) null, (LivingEntity) null, 0));
+    public static void renderItemIntoGUI(MatrixStack matrixStack, ItemStack stack, int x, int y, int z) {
+        renderItemModelIntoGUI(matrixStack, stack, x, y, z, Minecraft.getInstance().getItemRenderer().getModel(stack, (World) null, (LivingEntity) null));
     }
 
-    public static void renderItemAdvanced(PoseStack matrixStack, ItemStack stack, int x, int y, int z, String amount) {
-        renderItemModelIntoGUI(matrixStack, stack, x, y, z, Minecraft.getInstance().getItemRenderer().getModel(stack, (Level) null, (LivingEntity) null, 0));
+    public static void renderItemAdvanced(MatrixStack matrixStack, ItemStack stack, int x, int y, int z, String amount) {
+        renderItemModelIntoGUI(matrixStack, stack, x, y, z, Minecraft.getInstance().getItemRenderer().getModel(stack, (World) null, (LivingEntity) null));
         renderItemStackOverlay(matrixStack, Minecraft.getInstance().font, stack, x, y, amount, amount.length() - 2);
     }
 
-    public static void renderItemStackOverlay(PoseStack matrixStack, Font fr, ItemStack stack, int xPosition, int yPosition, @Nullable String text, int scaled) {
+    public static void renderItemStackOverlay(MatrixStack matrixStack, FontRenderer fr, ItemStack stack, int xPosition, int yPosition, @Nullable String text, int scaled) {
         matrixStack.pushPose();
         matrixStack.translate(0, 0, 512 + 32);
         if (!stack.isEmpty()) {
             if (stack.getCount() != 1 || text != null) {
                 String s = text == null ? String.valueOf(stack.getCount()) : text;
                 if (text == null && stack.getCount() < 1) {
-                    ChatFormatting var10000 = ChatFormatting.RED;
+                    TextFormatting var10000 = TextFormatting.RED;
                     s = var10000 + String.valueOf(stack.getCount());
                 }
 
@@ -75,34 +78,36 @@ public class TooltipUtil {
     }
 
     @OnlyIn(Dist.CLIENT)
-    public static void renderItemModelIntoGUI(PoseStack posestack, ItemStack stack, int x, int y, int z, BakedModel bakedmodel) {
-        Minecraft.getInstance().getTextureManager().getTexture(TextureAtlas.LOCATION_BLOCKS).setFilter(false, false);
-        RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_BLOCKS);
+    public static void renderItemModelIntoGUI(MatrixStack posestack, ItemStack stack, int x, int y, int z, IBakedModel bakedmodel) {
+        Minecraft.getInstance().getTextureManager().getTexture(PlayerContainer.BLOCK_ATLAS).setFilter(false, false);
+        Minecraft.getInstance().textureManager.bind(PlayerContainer.BLOCK_ATLAS);
         RenderSystem.enableBlend();
         RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         posestack.pushPose();
         posestack.translate((double) x, (double) y, (double) z);
         posestack.translate(8.0D, 8.0D, 0.0D);
         posestack.scale(1.0F, -1.0F, 1.0F);
         posestack.scale(16.0F, 16.0F, 16.0F);
-        RenderSystem.applyModelViewMatrix();
-        MultiBufferSource.BufferSource multibuffersource$buffersource = Minecraft.getInstance().renderBuffers().bufferSource();
+        IRenderTypeBuffer.Impl buffer = Minecraft.getInstance().renderBuffers().bufferSource();
         boolean flag = !bakedmodel.usesBlockLight();
         if (flag) {
-            Lighting.setupForFlatItems();
+            setupForFlatItems();
         }
-
-        Minecraft.getInstance().getItemRenderer().render(stack, ItemTransforms.TransformType.GUI, false, posestack, multibuffersource$buffersource, 15728880, OverlayTexture.NO_OVERLAY, bakedmodel);
-        multibuffersource$buffersource.endBatch();
+        Minecraft.getInstance().getItemRenderer().render(stack, ItemCameraTransforms.TransformType.GUI, false, posestack, buffer, 15728880, OverlayTexture.NO_OVERLAY, bakedmodel);
+        buffer.endBatch();
         RenderSystem.enableDepthTest();
         if (flag) {
-            Lighting.setupFor3DItems();
+            setupFor3DItems();
         }
-
         posestack.popPose();
-        RenderSystem.applyModelViewMatrix();
     }
 
+    public static void setupForFlatItems() {
+        RenderSystem.setupGuiFlatDiffuseLighting(DIFFUSE_LIGHT_0, DIFFUSE_LIGHT_1);
+    }
 
+    public static void setupFor3DItems() {
+        RenderSystem.setupGui3DDiffuseLighting(DIFFUSE_LIGHT_0, DIFFUSE_LIGHT_1);
+    }
 }
