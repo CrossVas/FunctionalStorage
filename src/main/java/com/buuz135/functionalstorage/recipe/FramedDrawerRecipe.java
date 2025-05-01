528533de3d8f734b9cf8e268fea063e479e9a3d1
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 21:31:57 +0900

    fixed: FramedDrawerRecipe

--- src/main/java/com/buuz135/functionalstorage/recipe/FramedDrawerRecipe.java
+++ src/main/java/com/buuz135/functionalstorage/recipe/FramedDrawerRecipe.java
@@ -1,23 +1,19 @@
 package com.buuz135.functionalstorage.recipe;
 
-
 import com.buuz135.functionalstorage.block.CompactingDrawerBlock;
 import com.buuz135.functionalstorage.block.FramedDrawerBlock;
-import com.google.common.collect.Lists;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.CraftingContainer;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.CustomRecipe;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.SimpleRecipeSerializer;
-import net.minecraft.world.level.Level;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.SpecialRecipe;
+import net.minecraft.item.crafting.SpecialRecipeSerializer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 
-import java.util.List;
+public class FramedDrawerRecipe extends SpecialRecipe {
 
-public class FramedDrawerRecipe extends CustomRecipe {
-
-    public static RecipeSerializer<FramedDrawerRecipe> SERIALIZER = new SimpleRecipeSerializer<>(FramedDrawerRecipe::new);
+    public static IRecipeSerializer<FramedDrawerRecipe> SERIALIZER = new SpecialRecipeSerializer<>(FramedDrawerRecipe::new);
 
     public FramedDrawerRecipe(ResourceLocation idIn) {
         super(idIn);
@@ -25,22 +21,20 @@ public class FramedDrawerRecipe extends CustomRecipe {
 
 
     public static boolean matches(ItemStack first, ItemStack second, ItemStack drawer) {
-        //System.out.println(((BlockItem) drawer.getItem()).getBlock().getClass());
         return !first.isEmpty() && first.getItem() instanceof BlockItem && !second.isEmpty() && second.getItem() instanceof BlockItem && !drawer.isEmpty() && drawer.getItem() instanceof BlockItem && ((BlockItem) drawer.getItem()).getBlock() instanceof FramedDrawerBlock;
     }
 
     public static boolean matchesCompacting(ItemStack first, ItemStack second, ItemStack drawer) {
-        //System.out.println(((BlockItem) drawer.getItem()).getBlock().getClass());
         return !first.isEmpty() && first.getItem() instanceof BlockItem && !second.isEmpty() && second.getItem() instanceof BlockItem && !drawer.isEmpty() && drawer.getItem() instanceof BlockItem && ((BlockItem) drawer.getItem()).getBlock() instanceof CompactingDrawerBlock;
     }
 
     @Override
-    public boolean matches(CraftingContainer inv, Level worldIn) {
+    public boolean matches(CraftingInventory inv, World world) {
         return matches(inv.getItem(0), inv.getItem(1), inv.getItem(2)) || matchesCompacting(inv.getItem(0), inv.getItem(1), inv.getItem(2));
     }
 
     @Override
-    public ItemStack assemble(CraftingContainer inv) {
+    public ItemStack assemble(CraftingInventory inv) {
         if (matches(inv.getItem(0), inv.getItem(1), inv.getItem(2)) || matchesCompacting(inv.getItem(0), inv.getItem(1), inv.getItem(2))){
             return FramedDrawerBlock.fill(inv.getItem(0), inv.getItem(1), inv.getItem(2).copy());
         }
@@ -53,7 +47,7 @@ public class FramedDrawerRecipe extends CustomRecipe {
     }
 
     @Override
-    public RecipeSerializer<?> getSerializer() {
+    public IRecipeSerializer<?> getSerializer() {
         return SERIALIZER;
     }
 }
