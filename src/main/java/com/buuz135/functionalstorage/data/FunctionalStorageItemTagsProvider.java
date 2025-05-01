4c9cce5e49f8d3bca1389ac78b28df9fa2a10ede
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 22:01:58 +0900

    fixed: FunctionalStorageItemTagsProvider

--- src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageItemTagsProvider.java
+++ src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageItemTagsProvider.java
@@ -2,29 +2,29 @@ package com.buuz135.functionalstorage.data;
 
 import com.buuz135.functionalstorage.FunctionalStorage;
 import com.buuz135.functionalstorage.util.StorageTags;
+import net.minecraft.block.Block;
+import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.tags.BlockTagsProvider;
-import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.data.tags.TagsProvider;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.data.ItemTagsProvider;
+import net.minecraft.data.TagsProvider;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.fml.RegistryObject;
 import org.apache.commons.lang3.tuple.Pair;
-import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nullable;
 import java.util.stream.Collectors;
 
 public class FunctionalStorageItemTagsProvider extends ItemTagsProvider {
 
-    public FunctionalStorageItemTagsProvider(DataGenerator p_126530_, BlockTagsProvider p_126531_, String modId, @Nullable ExistingFileHelper existingFileHelper) {
-        super(p_126530_, p_126531_, modId, existingFileHelper);
+    public FunctionalStorageItemTagsProvider(DataGenerator generator, BlockTagsProvider provider, String modId, @Nullable ExistingFileHelper existingFileHelper) {
+        super(generator, provider, modId, existingFileHelper);
     }
 
     @Override
     protected void addTags() {
-        TagsProvider.TagAppender<Item> tTagAppender = this.tag(StorageTags.DRAWER);
+        TagsProvider.Builder<Item> tTagAppender = this.tag(StorageTags.DRAWER);
         for (FunctionalStorage.DrawerType drawerType : FunctionalStorage.DRAWER_TYPES.keySet()) {
             for (RegistryObject<Block> blockRegistryObject : FunctionalStorage.DRAWER_TYPES.get(drawerType).stream().map(Pair::getLeft).collect(Collectors.toList())) {
                 tTagAppender.add(blockRegistryObject.get().asItem());
@@ -36,7 +36,6 @@ public class FunctionalStorageItemTagsProvider extends ItemTagsProvider {
                 .add(Items.MELON, Items.MELON_SLICE)
                 .add(Items.QUARTZ, Items.QUARTZ_BLOCK)
                 .add(Items.ICE, Items.BLUE_ICE, Items.PACKED_ICE)
-                .add(Items.AMETHYST_BLOCK, Items.AMETHYST_SHARD)
         ;
     }
 }
