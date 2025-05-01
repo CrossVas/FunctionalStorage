bc0a8b337bbc8f8f78b0de4abf1fbdc0488c5683
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 21:05:15 +0900

    fixed: EnderSavedData

--- src/main/java/com/buuz135/functionalstorage/world/EnderSavedData.java
+++ src/main/java/com/buuz135/functionalstorage/world/EnderSavedData.java
@@ -1,72 +1,66 @@
 package com.buuz135.functionalstorage.world;
 
 import com.buuz135.functionalstorage.inventory.EnderInventoryHandler;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.DimensionSavedDataManager;
+import net.minecraft.world.storage.WorldSavedData;
 
 import java.util.HashMap;
 
-public class EnderSavedData extends SavedData {
+public class EnderSavedData extends WorldSavedData {
 
-    public static EnderSavedData CLIENT = new EnderSavedData(null);
+    public static EnderSavedData CLIENT = new EnderSavedData();
 
     public static final String NAME = "FunctionalStorageEnder";
 
-    private HashMap<String, EnderInventoryHandler> itemHandlers;
+    private final HashMap<String, EnderInventoryHandler> itemHandlers = new HashMap<>();
 
-    private Level level;
-
-    public EnderSavedData(Level level) {
-        this.itemHandlers = new HashMap<>();
-        this.level = level;
+    public EnderSavedData() {
+        super(NAME);
     }
 
-    public static EnderSavedData getInstance(LevelAccessor accessor){
-        if (accessor instanceof ServerLevel){
-            ServerLevel serverWorld = ((ServerLevel) accessor).getServer().getLevel(Level.OVERWORLD);
-            EnderSavedData data = serverWorld.getDataStorage().computeIfAbsent(tag -> EnderSavedData.load(tag, (ServerLevel) accessor), () -> new EnderSavedData((ServerLevel)accessor), NAME);
-            return data;
-        } else if (accessor instanceof ClientLevel){
+    public static EnderSavedData getInstance(World world) {
+        if (world instanceof ServerWorld) {
+            ServerWorld serverWorld = (ServerWorld) world;
+            DimensionSavedDataManager storage = serverWorld.getDataStorage();
+
+            return storage.computeIfAbsent(
+                    EnderSavedData::new,
+                    NAME
+            );
+        } else {
             return CLIENT;
         }
-        return null;
-    }
-
-    private static EnderSavedData load(CompoundTag compoundTag, Level level) {
-        EnderSavedData manager = new EnderSavedData(level);
-        manager.itemHandlers = new HashMap<>();
-        CompoundTag backpacks = compoundTag.getCompound("Ender");
-        for (String s : backpacks.getAllKeys()) {
-            EnderInventoryHandler hander = new EnderInventoryHandler(s, manager);
-            hander.deserializeNBT(backpacks.getCompound(s));
-            manager.itemHandlers.put(s, hander);
-        }
-
-        return manager;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setFrenquency(String frequency, EnderInventoryHandler handler){
-        itemHandlers.put(frequency, handler);
     }
 
     @Override
-    public CompoundTag save(CompoundTag tag) {
-        CompoundTag nbt = new CompoundTag();
-        itemHandlers.forEach((s, iItemHandler) -> nbt.put(s, iItemHandler.serializeNBT()));
-        tag.put("Ender", nbt);
+    public void load(CompoundNBT tag) {
+        itemHandlers.clear();
+        CompoundNBT enderTag = tag.getCompound("Ender");
+        for (String key : enderTag.getAllKeys()) {
+            EnderInventoryHandler handler = new EnderInventoryHandler(key, this);
+            handler.deserializeNBT(enderTag.getCompound(key));
+            itemHandlers.put(key, handler);
+        }
+    }
+
+    @Override
+    public CompoundNBT save(CompoundNBT tag) {
+        CompoundNBT enderTag = new CompoundNBT();
+        itemHandlers.forEach((key, handler) -> enderTag.put(key, handler.serializeNBT()));
+        tag.put("Ender", enderTag);
         return tag;
     }
 
-    public EnderInventoryHandler getFrequency(String string){
-        return itemHandlers.computeIfAbsent(string, s -> new EnderInventoryHandler(s, this));
+    public void setFrequency(String frequency, EnderInventoryHandler handler) {
+        itemHandlers.put(frequency, handler);
+        setDirty(); // mark data as dirty so it gets saved
+    }
+
+    public EnderInventoryHandler getFrequency(String frequency) {
+        return itemHandlers.computeIfAbsent(frequency, s -> new EnderInventoryHandler(s, this));
     }
 
     public HashMap<String, EnderInventoryHandler> getItemHandlers() {
