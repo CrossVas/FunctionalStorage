9b3aabe401de98e1d6baa64d11a801579499268b
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-01 22:07:55 +0900

    fixed: FramedColors

--- src/main/java/com/buuz135/functionalstorage/client/FramedColors.java
+++ src/main/java/com/buuz135/functionalstorage/client/FramedColors.java
@@ -5,43 +5,42 @@ import com.buuz135.functionalstorage.block.CompactingFramedDrawerBlock;
 import com.buuz135.functionalstorage.block.FramedDrawerBlock;
 import com.buuz135.functionalstorage.block.tile.FramedDrawerTile;
 import com.buuz135.functionalstorage.client.model.FramedDrawerModelData;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.color.block.BlockColor;
-import net.minecraft.client.color.block.BlockColors;
-import net.minecraft.client.color.item.ItemColor;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.IBlockDisplayReader;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nullable;
 import java.util.Map;
-import java.util.Objects;
 
 @Mod.EventBusSubscriber(modid = FunctionalStorage.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class FramedColors implements BlockColor, ItemColor {
+public class FramedColors implements IBlockColor, IItemColor {
 
     @Override
-    public int getColor(BlockState state, @Nullable BlockAndTintGetter level, @Nullable BlockPos pos, int tintIndex) {
+    public int getColor(BlockState state, @Nullable IBlockDisplayReader level, @Nullable BlockPos pos, int tintIndex) {
         if (level != null && pos != null && tintIndex == 0) {
-            BlockEntity entity = level.getBlockEntity(pos);
-            if (entity instanceof FramedDrawerTile tile) {
+            TileEntity entity = level.getBlockEntity(pos);
+            if (entity instanceof FramedDrawerTile) {
+                FramedDrawerTile tile = (FramedDrawerTile) entity;
                 FramedDrawerModelData framedDrawerModelData = tile.getFramedDrawerModelData();
                 if (framedDrawerModelData != null) {
                     for (Map.Entry<String, Item> entry: framedDrawerModelData.getDesign().entrySet()) {
-                        if (entry.getValue() instanceof BlockItem blockItem) {
+                        if (entry.getValue() instanceof BlockItem) {
+                            BlockItem blockItem = (BlockItem) entry.getValue();
                             BlockState state1 = blockItem.getBlock().defaultBlockState();
                             int color = Minecraft.getInstance().getBlockColors().getColor(state1, level, pos, tintIndex);
                             if (color != -1)
@@ -57,11 +56,12 @@ public class FramedColors implements BlockColor, ItemColor {
     @Override
     public int getColor(ItemStack itemStack, int tintIndex) {
         if (tintIndex == 0) {
-            if (itemStack.getItem() instanceof BlockItem item && (item.getBlock() instanceof FramedDrawerBlock || item.getBlock() instanceof CompactingFramedDrawerBlock)) {
+            Item item = itemStack.getItem();
+            if (item instanceof BlockItem && (((BlockItem) item).getBlock() instanceof FramedDrawerBlock || ((BlockItem) item).getBlock() instanceof CompactingFramedDrawerBlock)) {
                 FramedDrawerModelData framedDrawerModelData = FramedDrawerBlock.getDrawerModelData(itemStack);
                 if (framedDrawerModelData != null) {
                     for (Map.Entry<String, Item> entry: framedDrawerModelData.getDesign().entrySet()) {
-                        if (entry.getValue() instanceof BlockItem blockItem) {
+                        if (entry.getValue() instanceof BlockItem) {
                             int color = Minecraft.getInstance().getItemColors().getColor(itemStack, tintIndex);
                             if (color != -1)
                                 return color;
