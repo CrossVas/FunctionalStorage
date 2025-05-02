4431ec83951d90f231a479625dd79f1c985174c5
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-02 12:27:39 +0900

    fixed: DrawerTile

--- src/main/java/com/buuz135/functionalstorage/block/tile/DrawerTile.java
+++ src/main/java/com/buuz135/functionalstorage/block/tile/DrawerTile.java
@@ -5,26 +5,26 @@ import com.buuz135.functionalstorage.client.gui.DrawerInfoGuiAddon;
 import com.buuz135.functionalstorage.inventory.BigInventoryHandler;
 import com.buuz135.functionalstorage.util.IWoodType;
 import com.hrznstudio.titanium.annotation.Save;
+import com.hrznstudio.titanium.api.IFactory;
+import com.hrznstudio.titanium.api.client.IScreenAddon;
 import com.hrznstudio.titanium.block.BasicTileBlock;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
-import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class DrawerTile extends ItemControllableDrawerTile<DrawerTile> {
 
@@ -34,7 +34,7 @@ public class DrawerTile extends ItemControllableDrawerTile<DrawerTile> {
     private FunctionalStorage.DrawerType type;
     private IWoodType woodType;
 
-    public DrawerTile(BasicTileBlock<DrawerTile> base, BlockEntityType<DrawerTile> blockEntityType, BlockPos pos, BlockState state, FunctionalStorage.DrawerType type, IWoodType woodType) {
+    public DrawerTile(BasicTileBlock<DrawerTile> base, TileEntityType<DrawerTile> blockEntityType, BlockPos pos, BlockState state, FunctionalStorage.DrawerType type, IWoodType woodType) {
         super(base, blockEntityType, pos, state);
         this.type = type;
         this.woodType = woodType;
@@ -74,17 +74,17 @@ public class DrawerTile extends ItemControllableDrawerTile<DrawerTile> {
         lazyStorage = LazyOptional.of(() -> this.handler);
     }
 
-    @OnlyIn(Dist.CLIENT)
     @Override
-    public void initClient() {
-        super.initClient();
-        addGuiAddonFactory(() -> new DrawerInfoGuiAddon(64, 16,
+    public List<IFactory<? extends IScreenAddon>> getScreenAddons() {
+        List<IFactory<? extends IScreenAddon>> screenAddons = super.getScreenAddons();
+        screenAddons.add(() -> new DrawerInfoGuiAddon(64, 16,
                 new ResourceLocation(FunctionalStorage.MOD_ID, "textures/blocks/" + woodType.getName() + "_front_" + type.getSlots() + ".png"),
                 type.getSlots(),
                 type.getSlotPosition(),
                 integer -> getHandler().getStackInSlot(integer),
                 integer -> getHandler().getSlotLimit(integer)
         ));
+        return screenAddons;
     }
 
     @Nonnull
@@ -98,7 +98,7 @@ public class DrawerTile extends ItemControllableDrawerTile<DrawerTile> {
 
     public ActionResultType onSlotActivated(PlayerEntity playerIn, Hand hand, Direction facing, double hitX, double hitY, double hitZ, int slot) {
         ItemStack stack = playerIn.getItemInHand(hand);
-        if (stack.getItem().equals(FunctionalStorage.CONFIGURATION_TOOL.get()) || stack.getItem().equals(FunctionalStorage.LINKING_TOOL.get())) return InteractionResult.PASS;
+        if (stack.getItem().equals(FunctionalStorage.CONFIGURATION_TOOL.get()) || stack.getItem().equals(FunctionalStorage.LINKING_TOOL.get())) return ActionResultType.PASS;
         if (slot != -1 && !playerIn.getItemInHand(hand).isEmpty()){
             BigInventoryHandler.BigStack bigStack = getHandler().getStoredStacks().get(slot);
             if (bigStack.getStack().isEmpty()){
@@ -108,7 +108,7 @@ public class DrawerTile extends ItemControllableDrawerTile<DrawerTile> {
         return super.onSlotActivated(playerIn, hand, facing, hitX, hitY, hitZ, slot);
     }
 
-    @NotNull
+    @Nonnull
     @Override
     public DrawerTile getSelf() {
         return this;
