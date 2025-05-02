7877280c3f08970f373959a3d6353587ad1e7926
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-02 12:40:50 +0900

    fixed: ArmoryCabinetTile

--- src/main/java/com/buuz135/functionalstorage/block/tile/ArmoryCabinetTile.java
+++ src/main/java/com/buuz135/functionalstorage/block/tile/ArmoryCabinetTile.java
@@ -4,18 +4,14 @@ import com.buuz135.functionalstorage.inventory.ArmoryCabinetInventoryHandler;
 import com.hrznstudio.titanium.annotation.Save;
 import com.hrznstudio.titanium.block.BasicTileBlock;
 import com.hrznstudio.titanium.block.tile.ActiveTile;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.Connection;
-import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
+import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
-import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -26,8 +22,8 @@ public class ArmoryCabinetTile extends ActiveTile<ArmoryCabinetTile> {
     public ArmoryCabinetInventoryHandler handler;
     private final LazyOptional<IItemHandler> lazyStorage;
 
-    public ArmoryCabinetTile(BasicTileBlock<ArmoryCabinetTile> base, BlockEntityType<?> entityType, BlockPos pos, BlockState state) {
-        super(base, entityType, pos, state);
+    public ArmoryCabinetTile(BasicTileBlock<ArmoryCabinetTile> base) {
+        super(base);
         this.handler = new ArmoryCabinetInventoryHandler() {
             @Override
             public void onChange() {
@@ -55,20 +51,19 @@ public class ArmoryCabinetTile extends ActiveTile<ArmoryCabinetTile> {
     }
 
     @Override
-    public ClientboundBlockEntityDataPacket getUpdatePacket() {
-        return ClientboundBlockEntityDataPacket.create(this, blockEntity -> new CompoundTag());
+    public SUpdateTileEntityPacket getUpdatePacket() {
+        return new SUpdateTileEntityPacket(getBlockPos(), 0, new CompoundNBT());
     }
 
     @Override
-    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
-        //super.onDataPacket(net, pkt);
+    public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+//        super.onDataPacket(net, pkt);
     }
 
     @Override
     @Nonnull
-    public CompoundTag getUpdateTag() {
-        CompoundTag compoundTag = new CompoundTag();
-        return compoundTag;
+    public CompoundNBT getUpdateTag() {
+        return new CompoundNBT();
     }
 
     public boolean isEverythingEmpty() {
@@ -80,7 +75,7 @@ public class ArmoryCabinetTile extends ActiveTile<ArmoryCabinetTile> {
         return true;
     }
 
-    @NotNull
+    @Nonnull
     @Override
     public ArmoryCabinetTile getSelf() {
         return this;
