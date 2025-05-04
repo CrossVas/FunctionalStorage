c39162ef41fc7b280ff376dbbd2b7d230521caf8
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-05 03:46:00 +0900

    feat: added more configs

--- src/main/java/com/buuz135/functionalstorage/block/config/FunctionalStorageConfig.java
+++ src/main/java/com/buuz135/functionalstorage/block/config/FunctionalStorageConfig.java
@@ -6,6 +6,10 @@ import com.hrznstudio.titanium.annotation.config.ConfigVal;
 @ConfigFile(value = "functionalstorage-common")
 public class FunctionalStorageConfig {
 
+    @ConfigVal(comment = "Every how many ticks the drawer upgrades will work")
+    @ConfigVal.InRangeInt(min = 1)
+    public static int UPGRADE_TICK = 4;
+
     @ConfigVal(comment = "Armory slot amount")
     @ConfigVal.InRangeInt(min = 1)
     public static int ARMORY_CABINET_SIZE = 4096;
@@ -13,4 +17,18 @@ public class FunctionalStorageConfig {
     @ConfigVal(comment = "Linking range radius")
     public static int DRAWER_CONTROLLER_LINKING_RANGE = 12;
 
+    @ConfigVal(comment = "How many items the collector upgrade will try to pull")
+    public static int UPGRADE_COLLECTOR_ITEMS = 4;
+
+    @ConfigVal(comment = "How many items the pushing upgrade will try to pull")
+    public static int UPGRADE_PUSH_ITEMS = 4;
+
+    @ConfigVal(comment = "How many items the pulling upgrade will try to pull")
+    public static int UPGRADE_PULL_ITEMS = 4;
+
+    @ConfigVal(comment = "How much fluid (in mb) the pulling upgrade will try to pull")
+    public static int UPGRADE_PULL_FLUID = 500;
+
+    @ConfigVal(comment = "How much fluid (in mb) the pushing upgrade will try to pull")
+    public static int UPGRADE_PUSH_FLUID = 500;
 }
